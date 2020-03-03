7ad4cb93c5102898dcf12da86b586202d62d0287
Matthias Gruhler <matthias.gruhler@gmail.com>
2020-03-03 12:58:36 +0100

    update keys for ros package sources

--- 16.04_xenial_kinetic_amd64/bootstrap.sh
+++ 16.04_xenial_kinetic_amd64/bootstrap.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-ut
 
 # setup ROS stuff
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
 sudo apt-get install -y ros-kinetic-desktop-full
 sudo apt-get install -y python-rosinstall 
--- 18.04_bionic_melodic_amd64/bootstrap.sh
+++ 18.04_bionic_melodic_amd64/bootstrap.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-ut
 
 # setup ROS stuff
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
 sudo apt-get install -y ros-melodic-ros-base python-catkin-tools python-catkin-lint
 sudo apt-get install -y gitg vim meld terminator
