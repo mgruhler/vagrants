abd4dfd94db4a130e8691cceeeabbaf667558f99
Matthias Gruhler <matthias.gruhler@gmail.com>
2020-06-16 17:01:42 +0200

    20.04 uses python3, duh...

--- 20.04_focal_noetic_amd64/bootstrap.sh
+++ 20.04_focal_noetic_amd64/bootstrap.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-ut
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
-sudo apt-get install -y ros-noetic-ros-base python-catkin-tools python-catkin-lint
+sudo apt-get install -y ros-noetic-ros-base python3-catkin-tools python3-catkin-lint
 sudo apt-get install -y gitg vim meld terminator
 sudo rosdep init
 sudo reboot
